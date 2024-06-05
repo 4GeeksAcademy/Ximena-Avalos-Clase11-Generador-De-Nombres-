89b268988271bdf62a113d0ca25b2d9e0a4bcb78
XimenaAvalos <164333573+XimenaAvalos@users.noreply.github.com>
2024-06-05 11:02:02 +0000

    VersionFinal

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //Mi codigo
+
+  function forLoopArreglo() {
+    const pronoun = ["the", "our"];
+    const adj = ["great", "big"];
+    const noun = ["jogger", "racoon"];
+    const extensions = [".com", ".net", ".us", ".io"];
+
+    console.log("Lista de combinaciones de dominios:");
+
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let e = 0; e < extensions.length; e++) {
+            console.log(pronoun[p] + adj[a] + noun[n] + extensions[e]);
+          }
+        }
+      }
+    }
+  }
+
+  forLoopArreglo();
 };
